f0d16737ab04b95e3e1cd934726d8dcd1f4ec564
Hogan <418730859@qq.com>
2020-11-10 01:28:03 +0800

    扩展解决Express的类型定义文件问题

--- ts-crowller/src/index.ts
+++ ts-crowller/src/index.ts
@@ -1,8 +1,14 @@
-import express, { Request, Response} from 'express'
+import express, {NextFunction, Request, Response} from 'express'
+import bodyParser from 'body-parser'
 import router from './router'
 
+// 问题2：当我使用中间件的时候，对 req 或者 res 做了修改之后，实际上类型并不能改变
 const app = express()
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.teacherName = 'dell'
+  next()
+})
 app.use(router)
 
 app.listen(7001, () => {
--- ts-crowller/src/router.ts
+++ ts-crowller/src/router.ts
@@ -2,19 +2,41 @@ import {Request, Response, Router} from "express";
 import Crowller from "./crowller";
 import DellAnalyzer from './dellAnalyzer';
 
+// 问题1：express 库的类型定义文件，.d.ts 文件类型描述不准确
+interface RequestWithBody extends Request{
+  body: {
+    [key: string]: string | undefined
+  }
+}
+
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('hello, world')
+  res.send(`
+    <html>
+      <body>
+        <form method="post" action="/getData">
+          <input type="password" name="password"/>
+          <button>提交</button>
+        </form>
+      </body>
+    </html>
+  `)
 })
 
-router.get('/getData', (req: Request, res: Response) => {
-  const secret = 'x3b174jsx';
-  const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+router.post('/getData', (req: RequestWithBody, res: Response) => {
+  const { password } = req.body
+  if (password === '123') {
+    const secret = 'x3b174jsx';
+    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
 
-  const analyzer = DellAnalyzer.getInstance();
-  new Crowller(url, analyzer);
-  res.send('getData success')
+    const analyzer = DellAnalyzer.getInstance();
+    new Crowller(url, analyzer);
+    res.send('getData success')
+  } else {
+    res.send('password Error!')
+  }
+
 })
 
 
